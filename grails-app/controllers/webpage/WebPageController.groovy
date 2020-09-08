a65e652a603f1f5613f62cc32360f10258a645a6
Abdus_Salam <44572181+ABDUSSALAM1502036@users.noreply.github.com>
2020-09-08 17:41:02 +0600

    first commit

---
+++ grails-app/controllers/webpage/WebPageController.groovy
@@ -0,0 +1,14 @@
+package webpage
+
+class WebPageController {
+
+    def index() { }
+    def Menu() { }
+    def about() { }
+    def contact() { }
+    def blog() { }
+    def blog_details() { }
+    def reservation() { }
+    def stuff() { }
+    def gallery() { }
+}
